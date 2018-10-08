b6b1b7f7574d880091e4013d0548fa1cbca3ecde
rasmusandre <rasmusandre@gmail.com>
2018-10-08 16:28:37 +0200

    Update day 1

---
+++ build/CMakeFiles/mandel.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/rasmus/Skole/parallelle_beregninger/TDT4200-Assignment-3/src/main.cpp" "/home/rasmus/Skole/parallelle_beregninger/TDT4200-Assignment-3/build/CMakeFiles/mandel.dir/src/main.cpp.o"
+  "/home/rasmus/Skole/parallelle_beregninger/TDT4200-Assignment-3/src/utilities/lodepng.cpp" "/home/rasmus/Skole/parallelle_beregninger/TDT4200-Assignment-3/build/CMakeFiles/mandel.dir/src/utilities/lodepng.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "PROJECT_SOURCE_DIR=\"/home/rasmus/Skole/parallelle_beregninger/TDT4200-Assignment-3\""
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../src"
+  "/usr/lib/openmpi/include/openmpi/opal/mca/event/libevent2021/libevent"
+  "/usr/lib/openmpi/include/openmpi/opal/mca/event/libevent2021/libevent/include"
+  "/usr/lib/openmpi/include"
+  "/usr/lib/openmpi/include/openmpi"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/mandel.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mandel.dir/src/main.cpp.o"
+  "CMakeFiles/mandel.dir/src/utilities/lodepng.cpp.o"
+  "mandel/mandel.pdb"
+  "mandel/mandel"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mandel.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
